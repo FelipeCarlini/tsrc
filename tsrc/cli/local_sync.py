630b0708ec40f409918cc799b2654b0227986d3e
Felipe Rafael Carlini <felipercarlini@gmail.com>
2020-09-08 11:15:40 -0300

    way that config file is writen

--- tsrc/cli/local_sync.py
+++ tsrc/cli/local_sync.py
@@ -1,11 +1,13 @@
 """ Entry point for `tsrc sync` """
 
+import subprocess
 from typing import List, Optional
 import cli_ui as ui
 from argh import arg
 from path import Path
 import shutil
 import os
+from tsrc.workspace.config import WorkspaceConfig
 
 from tsrc.cli import (
     get_workspace,
@@ -22,6 +24,26 @@ def local_sync(
     force: bool = False,
 ) -> None:
     """ synchronize the current workspace with the manifest """
+
+    try:
+        remote_url = subprocess.check_output('git config --get remote.origin.url', shell=True).decode('utf-8').replace('\n', '')
+        cmd_string = "git rev-parse --abbrev-ref HEAD"
+        remote_branch = subprocess.check_output(cmd_string, shell=True).decode('utf-8').replace('\n', '')
+    except Exception:
+        remote_url = False
+        remote_branch = False
+
+    workspace_config = WorkspaceConfig(
+        manifest_url=remote_url,
+        manifest_branch=remote_branch,
+        clone_all_repos=False,
+        repo_groups=[],
+        shallow_clones=False,
+        singular_remote=None,
+    )
+    cfg_path = Path(os.getcwd()) / ".tsrc" / "config.yml"
+    workspace_config.save_to_file(cfg_path)
+
     workspace = get_workspace(workspace_path, local_file=True)
     update_manifest_local()
 
